bc1c437ab2df4398310d709e96891d10a6460f5b
ArduinoTechsDev <9836910+ToonAutomation@users.noreply.github.com>
2018-05-29 17:41:01 +0700

    Create TaskProcess.h

---
+++ TaskProcess.h
@@ -0,0 +1,17 @@
+
+class TaskProcess
+{
+public:
+		typedef void(*callback)(void);
+		void init(unsigned long interval_ms,callback function);		
+virtual	bool Run();
+		bool Enable(bool State) { RunState = State; };
+		void SetInterval(unsigned long interval) { Tinterval = interval; };
+		unsigned long GetInterval() { return Tinterval; };
+	    
+private:
+		
+		unsigned long TStart, Tinterval;		
+		callback  jobprocess;
+		bool RunState = false;
+};
